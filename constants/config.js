53ec6ace0300223f461cf6a263071cf0dbfd37c9
unknown <narwepintu@gmail.com>
2024-12-14 15:14:30 +0530

    update code

--- constants/config.js
+++ constants/config.js
@@ -1,16 +1,3 @@
-// const corsOptions={
-//     origin:[
-//         "http://localhost:5173",
-//         "http://localhost:4173",
-//         process.env.CLIENT_URL,
-//     ],
-//     methods:["GET","POST","PUT","DELETE"],
-//     credentials:true,
-// };
-// const CHATTU_TOKEN="chattu-token"
-
-// export {corsOptions,CHATTU_TOKEN}
-
 const corsOptions = {
     origin: [
       "http://localhost:5173",
--- utils/utility.js
+++ utils/utility.js
@@ -1,12 +1,3 @@
-// class ErrorHandler extends Error{
-//     constructor(message,statusCode){
-// super(message);
-// this.statusCode=statusCode;
-//     }
-// }
-
-// export {ErrorHandler}
-
 class ErrorHandler extends Error {
     constructor(message, statusCode) {
       super(message);
